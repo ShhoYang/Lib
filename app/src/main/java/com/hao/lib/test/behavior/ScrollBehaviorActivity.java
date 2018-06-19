8cd63ccd124a0cbd724dd8a58966aab0cdcee756
haoshiy <haoyshi@163.com>
2018-06-19 17:50:01 +0800

    添加了Dagger2

---
+++ app/src/main/java/com/hao/lib/test/behavior/ScrollBehaviorActivity.java
@@ -0,0 +1,46 @@
+package com.hao.lib.test.behavior;
+
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.hao.lib.R;
+import com.hao.lib.adapter.NewsAdapter;
+import com.hao.lib.base.ui.BaseActivity;
+import com.hao.lib.bean.News;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+
+public class ScrollBehaviorActivity extends BaseActivity {
+
+    @BindView(R.id.rv)
+    RecyclerView mRecyclerView;
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_scroll_behavior;
+    }
+
+    @Override
+    public void initInject() {
+
+    }
+
+    @Override
+    public void initView() {
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        List<News> list = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            list.add(new News(i + ""));
+        }
+
+        mRecyclerView.setAdapter(new NewsAdapter(R.layout.item_main, list));
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
